6fdb908df4d455e389d238bf16f49123deb310d8
s27944 <s27944@pjwstk.edu.pl>
2024-04-04 20:26:28 +0200

    Zrefaktoryzowana została klasa UserService

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -6,19 +6,17 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            if (IsFirstNameCorrect(firstName) || IsLastNameCorrect(lastName))
             {
                 return false;
             }
 
-            if (!email.Contains("@") && !email.Contains("."))
+            if (!IsEmailCorrect(email))
             {
                 return false;
             }
 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            var age = CalulateAge(dateOfBirth);
 
             if (age < 21)
             {
@@ -60,13 +58,43 @@ namespace LegacyApp
                 }
             }
 
+            if (!CheckUserCredit(user)) return false;
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private static bool CheckUserCredit(User user)
+        {
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
                 return false;
             }
 
-            UserDataAccess.AddUser(user);
             return true;
+        }
+
+        private static int CalulateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            return age;
+        }
+
+        private static bool IsEmailCorrect(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+        private static bool IsLastNameCorrect(string lastName)
+        {
+            return string.IsNullOrEmpty(lastName);
+        }
+
+        private static bool IsFirstNameCorrect(string firstName)
+        {
+            return string.IsNullOrEmpty(firstName);
         }
     }
 }
